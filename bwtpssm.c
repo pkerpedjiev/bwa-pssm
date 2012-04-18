ac4a2e2b6927abaf54377f2970c3854e4ebf5b04
Peter Kerpedjiev <pkerp@plastilin.tbi.univie.ac.at>
2012-04-18 18:35:16 +0200

    Added a short readme.

--- bwtpssm.c
+++ bwtpssm.c
@@ -364,7 +364,7 @@ int bwa_pssm(int argc, char *argv[])
 
     if (optind + 2 > argc) {
         fprintf(stderr, "\n");
-        fprintf(stderr, "Usage:   bwa aln [options] <prefix> <in.fq>\n\n");
+        fprintf(stderr, "Usage:   bwa pssm [options] <prefix> <in.fq>\n\n");
         fprintf(stderr, "Options: -n NUM    max #diff (int) or missing prob under %.2f err rate (float) [%.2f]\n",
                 BWA_AVG_ERR, opt->fnr);
         fprintf(stderr, "         -o INT    maximum number (int) or fraction of gap opens [%d]\n", opt->max_gapo);
